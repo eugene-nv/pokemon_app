07cdab8d4ed5cf2c52fc3c34c7fbb989a7ddcb8b
eugene-nv <naumkin.e.v@yandex.ru>
2024-03-02 15:40:48 +0500

    add celery

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.11.4-alpine3.17
 COPY requirements.txt /temp/requirements.txt
 COPY pokemon_app /pokemon_app
 
-WORKDIR /pokemon
+WORKDIR /pokemon_app
 EXPOSE 8000
 
 RUN pip install -r /temp/requirements.txt
---
+++ pokemon_app/pokemon_app/celery_app.py
@@ -0,0 +1,16 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemon_app.settings')
+
+app = Celery('pokemon_app')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
+
+@app.task()
+def task():
+    print('Hello from task')
